b5ae4cf3fa51e5be58e49aed123be86a277816f4
PapaPanda-Fedora <rafaeljonathan.work@gmail.com>
2023-07-12 15:02:23 -0400

    refactor search bar

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+const SearchBar = () => {
+  return (
+    <div className="relative">
+      <input
+        placeholder="Search"
+        className="py-3 pr-3 pl-11 rounded-3xl w-80 placeholder:text-[#4B5563]"
+      />
+      <Image
+        src="/assets/icon/search-icon.svg"
+        width={20}
+        height={20}
+        alt="search icon"
+        className="absolute top-[14px] left-3"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
