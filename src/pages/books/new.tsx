22331d27081286974b80e88f4613fbbe359a9a1c
PapaPanda-Fedora <rafaeljonathan.work@gmail.com>
2023-07-13 11:47:40 -0400

    add create new book

--- src/pages/books/new.tsx
+++ src/pages/books/new.tsx
@@ -2,8 +2,11 @@ import { Form } from 'react-hook-form';
 
 const Page = () => {
   return (
-    <main>
-      <p>seeyou tummorow</p>
-    </main>
+    <>
+      <main>
+        <p>Hellow, See Yu Tumuruw</p>
+      </main>
+    </>
   );
 };
+export default Page;
