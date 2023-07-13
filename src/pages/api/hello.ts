348b8720bbcfa37fbf8f21e811900f9dfcb1e2da
Aldiansyah Dwi P <aldiansyhdp@gmail.com>
2023-07-13 08:46:50 +0700

    commit temp

---
+++ src/pages/api/hello.ts
@@ -0,0 +1,9 @@
+import { prisma } from '@/libs/db';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  res.status(200).json({ message: 'hello' });
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { signOut } from 'next-auth/react';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -6,6 +7,12 @@ export default function Home() {
   return (
     <main className="block">
       <h1>Home</h1>
+      <button
+        className="p-2 rounded-md bg-gray-400"
+        onClick={() => signOut({ callbackUrl: '/login' })}
+      >
+        Sign out
+      </button>
     </main>
   );
 }
