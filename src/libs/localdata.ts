05f46e511bee0bf39328707a0fbeebb49268b068
Rafael PraditaMac <rafaeljonathan.work@gmail.com>
2023-07-13 12:35:08 +0700

    wrong file path fix

--- src/libs/localdata.ts
+++ src/libs/localdata.ts
@@ -5,7 +5,7 @@ import fsPromises from 'fs/promises';
 import path from 'path';
 
 const getLocalData = async () => {
-  const filePath = path.join(process.cwd(), 'json/data.json');
+  const filePath = path.join(process.cwd(), 'src/json/dummyData.json');
   const jsonData = await fsPromises.readFile(filePath, 'utf-8');
   const objectData = JSON.parse(jsonData);
   return objectData;
