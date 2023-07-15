2e127cf2460a41086075d87b8d84ec4e1ee8f77f
PapaPanda-Fedora <rafaeljonathan.work@gmail.com>
2023-07-15 15:40:25 -0400

    refactoring with bug

--- src/pages/books/new.tsx
+++ src/pages/books/new.tsx
@@ -3,6 +3,10 @@ import { prisma } from '@/libs/db';
 import { GetServerSideProps } from 'next';
 import { IFeed } from '../home';
 import BookForm from '@/components/BookForm';
+import * as yup from 'yup';
+import axios from 'axios';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const feed = await prisma.book.findMany();
@@ -20,6 +24,17 @@ export const getServerSideProps: GetServerSideProps = async () => {
   };
 };
 
+const schema = yup.object({
+  title: yup.string().required('Please fill in the book title'),
+  writer: yup.string().required('Please fill in the writter'),
+  genres: yup.string().required('Please specified the genre'),
+  coverImgUrl: yup.string().required('Please find an image for the cover'),
+  done: yup.boolean(),
+  notes: yup
+    .string()
+    .required('Please tell us about your first impression about this book'),
+});
+
 export interface IFormInput {
   title: string;
   writer: string;
@@ -35,21 +50,47 @@ interface INewBookPageProps {
   post: IFormInput;
 }
 
-const NewContactPage = ({ feed, post }: INewBookPageProps) => {
+const NewBookPage = ({ feed, post }: INewBookPageProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+
+    //this bug learn from https://github.com/DefinitelyTyped/DefinitelyTyped/issues/29412 for implementation
+  } = useForm<IFormInput>({ resolver: yupResolver(schema), mode: 'onTouched' });
+
+  const onSubmit: SubmitHandler<IFormInput> = async (formData) => {
+    try {
+      console.log({ formData }, 'formdata');
+      // await axios.post(
+      //   '/api/books',
+      //   {
+      //     ...formData,
+      //     userId: session.data?.user.id,
+      //   },
+      //   {
+      //     headers: {
+      //       'Content-Type': 'application/json',
+      //     },
+      //   },
+      // );
+    } catch (error: any) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <NavBar data={feed} />
       <main></main>
       <BookForm
-        handleSubmit
-        onSubmit
-        errors
-        register
-        isSubmitting
+        handleSubmit={handleSubmit}
+        onSubmit={onSubmit}
+        errors={errors}
+        register={register}
         data={post}
         type="create"
       />
     </>
   );
 };
-export default NewContactPage;
+export default NewBookPage;
