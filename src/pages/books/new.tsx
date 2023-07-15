2cbbf2e1f20f64dc951428c37492af58ae575156
PapaPanda-Fedora <rafaeljonathan.work@gmail.com>
2023-07-15 08:02:45 -0400

    testing the form props

--- src/pages/books/new.tsx
+++ src/pages/books/new.tsx
@@ -1,21 +1,55 @@
 import NavBar from '@/components/NavBar';
-import { Form } from 'react-hook-form';
+import { prisma } from '@/libs/db';
+import { GetServerSideProps } from 'next';
 import { IFeed } from '../home';
+import BookForm from '@/components/BookForm';
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const feed = await prisma.book.findMany();
+  const post = {
+    id: '',
+    title: '',
+    writer: '',
+    genres: '',
+    coverImgUrl: '',
+    notes: '',
+    done: false,
+  };
+  return {
+    props: { feed, post },
+  };
+};
 
 export interface IFormInput {
   title: string;
   writer: string;
-  genres: boolean;
+  genres: string;
   coverImgUrl: string;
   notes: string;
   userId: string;
+  done: boolean;
 }
 
 interface INewBookPageProps {
   feed: IFeed[];
+  post: IFormInput;
 }
 
-const NewContactPage = ({ feed }: INewBookPageProps) => {
-  return <NavBar data={feed} />;
+const NewContactPage = ({ feed, post }: INewBookPageProps) => {
+  return (
+    <>
+      <NavBar data={feed} />
+      <main></main>
+      <BookForm
+        handleSubmit
+        onSubmit
+        errors
+        register
+        isSubmitting
+        data={post}
+        type="create"
+      />
+    </>
+  );
 };
 export default NewContactPage;
