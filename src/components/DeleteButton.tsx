53454604294f38949ee3e2eed1f6c9419cdfe548
winona ivana <winonaivana@gmail.com>
2023-07-16 21:51:37 +0100

    update delete components

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -1,17 +1,23 @@
 import Image from 'next/image';
 
-const DeleteButton = () => {
+type Props = { onDelete: () => void };
+
+const DeleteButton = ({ onDelete }: Props) => {
   return (
-    <div className="flex px-3 py-1 mt-4 rounded-md bg-red-600/20 border-2 border-red-700 max-w-fit">
-      <Image
-        src="/assets/icon/delete-icon.svg"
-        width={14}
-        height={14}
-        alt="done icon"
-        className="mr-2"
-      />
-      <p className="text-red-700 text-sm">Delete</p>
-    </div>
+    <button
+      onClick={onDelete}
+      className="flex px-3 py-1 mt-4 rounded-md bg-red-600/20 border-2 border-red-700 max-w-fit"
+    >
+      <div className="flex gap-2">
+        <Image
+          src="/assets/icon/delete-icon.svg"
+          width={14}
+          height={14}
+          alt="done icon"
+        />
+        <p className="text-red-700 text-sm">Delete</p>
+      </div>
+    </button>
   );
 };
 
