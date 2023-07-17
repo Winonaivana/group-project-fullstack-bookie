63c614a40f587f2018c9330a4bb147ed2f97d82e
winona ivana <winonaivana@gmail.com>
2023-07-17 15:21:29 +0100

    update book detail

--- src/pages/books/[id]/index.tsx
+++ src/pages/books/[id]/index.tsx
@@ -1,33 +1,96 @@
-import { GetServerSideProps, GetServerSidePropsContext } from 'next';
+import DeleteButton from '@/components/DeleteButton';
+import EditButton from '@/components/EditButton';
+import NavBar from '@/components/NavBar';
 import { prisma } from '@/libs/db';
-import { Book } from '@prisma/client';
 
-export const getServerSideProps: GetServerSideProps = async ({
-  params,
-}: GetServerSidePropsContext) => {
-  const book = await prisma.book.findFirst({
+import { IFeed } from '@/pages/home';
+
+import { GetServerSideProps } from 'next';
+import Image from 'next/image';
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const books = await prisma.book.findMany();
+  const book = await prisma.book.findUnique({
     where: {
       id: params?.id as string,
     },
   });
-
   return {
-    props: { book },
+    props: {
+      books,
+      book,
+    },
   };
 };
 
-interface IBookProps {
-  book: Book;
+interface BookProp {
+  books: IFeed[];
+
+  book: IFeed;
 }
 
-const Page = ({ book }: IBookProps) => {
+const Book = ({ book, books }: BookProp) => {
+  const handleDelete = () => {};
   return (
-    <main>
-      <p>{book.title}</p>
-      <p>{book.writer}</p>
-      <p className="test">{book.notes}</p>
-    </main>
+    <>
+      <NavBar data={books}></NavBar>
+      <div className="mt-8 flex justify-center items-center">
+        <div>
+          <div className="flex left-items-wrapper">
+            <Image
+              src="/assets/icon/book-icon.svg"
+              width={32.5}
+              height={27.5}
+              alt="file basket icon"
+            />
+            <h1 className="text-[32px] font-medium ml-3 ">Book Details</h1>
+          </div>
+          <div className="flex  mt-11 gap-6 max-w-2xl">
+            <div>
+              <div className="min-w-[200px]">
+                <img src={book.coverImgUrl} alt=""></img>
+              </div>
+              <div className="flex gap-4 justify-center">
+                <EditButton></EditButton>
+                <DeleteButton onDelete={handleDelete}></DeleteButton>
+              </div>
+            </div>
+            <div>
+              <h1 className=" text-4xl font-semibold">{book.title}</h1>
+              <div className="flex gap-5">
+                <div className="flex mt-4 author-wrapper">
+                  <Image
+                    src="/assets/icon/writer-icon.svg"
+                    width={14}
+                    height={14}
+                    alt="author icon"
+                    className="mr-2 "
+                  />{' '}
+                  <p>{book.writer}</p>
+                </div>
+                <div className="flex px-3 py-1 mt-4 rounded-md bg-sky-600/20 max-w-fit">
+                  <Image
+                    src="/assets/icon/in-progress-icon.svg"
+                    width={14}
+                    height={14}
+                    alt="done icon"
+                    className="mr-2"
+                  />
+                  <p className="text-sky-600 text-sm">In Progress</p>
+                </div>
+              </div>
+              <div className="px-3 py-1 mt-2 rounded-md bg-gray-300 max-w-fit">
+                <p className="text-gray-700 text-sm">{book.genres}</p>
+              </div>
+              <div className="mt-2">
+                <p>{book.notes}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
-export default Page;
+export default Book;
