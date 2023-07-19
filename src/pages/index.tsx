e35c764310bd9f786fcfb5e681724da47e1ee3fb
PapaPanda-Fedora <rafaeljonathan.work@gmail.com>
2023-07-18 21:05:35 -0400

    home migrate, fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,37 @@
 import { Inter } from 'next/font/google';
-
 import NavBar from '@/components/NavBar';
-import { GetServerSideProps } from 'next';
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import Image from 'next/image';
 import BookCard from '@/components/BookCard';
 import { prisma } from '@/libs/db';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const feed = await prisma.book.findMany();
+const inter = Inter({ subsets: ['latin'] });
+
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext,
+) => {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  if (!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/login',
+      },
+    };
+  }
+
+  const feed = await prisma.book.findMany({
+    where: {
+      userId: session.user.id,
+    },
+  });
   return {
     props: { feed },
   };
 };
-
-const inter = Inter({ subsets: ['latin'] });
 
 export interface IFeed {
   id: string;
@@ -44,9 +62,11 @@ const Home = ({ feed }: IHomeProps) => {
               alt="file basket icon"
             />
             <h1 className="text-[32px] font-medium ml-3 ">My books</h1>
-            <p className="px-5 py-3 rounded-full ml-7 bg-gray-900/20">2 Done</p>
+            <p className="px-5 py-3 rounded-full ml-7 bg-gray-900/20">
+              {feed.filter((value) => value.done === true).length} Done
+            </p>
             <p className="px-5 py-3 ml-3 rounded-full bg-gray-900/20">
-              10 In Progress
+              {feed.filter((value) => value.done === false).length} In Progress
             </p>
           </div>
           <button className="px-5 py-3 border rounded-full border-emerald-500 bg-emerald-500/20 text-emerald-500">
